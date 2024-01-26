0b8bf369eb296642846648fe9d8a61038fe4d3e8
Matt Rice <matthew.rice.d@gmail.com>
2024-01-25 22:17:24 -0500

    Cooler Demo scene in about

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Canvas } from "@react-three/fiber";
 
 import { Box } from "./components/Box";
 import { Physics, RigidBody } from "@react-three/rapier";
+import { OfficeScene } from "./components/OfficeScene";
 const App = () => {
   return (
-    <Canvas style={{ width: "100%", height: "100vh" }}>
+    <Canvas style={{ width: "100%", height: "100vh" }} gl={{antialias:true}}>
       <Physics>
         <ambientLight intensity={Math.PI / 2} />
         <spotLight
@@ -17,18 +18,19 @@ const App = () => {
         />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <RigidBody>
-          <Box position={[-1.2, 0, 0]}></Box>
+          <Box position={[-1.2, 1, 0]}></Box>
         </RigidBody>
-        <Box position={[1.2, 0, 0]} />
+        <Box position={[1.2, 1, 0]} />
         <OrbitControls />
         <RigidBody type="fixed">
           <Plane
             scale={[10, 10, 10]}
             rotation={[-(Math.PI/2), 0, 0]}
-            position={[0, -1, 0]}
+            position={[0, 0, 0]}
           ></Plane>
         </RigidBody>
       </Physics>
+      <OfficeScene position={[0,0,10]}/>
     </Canvas>
   );
 };
