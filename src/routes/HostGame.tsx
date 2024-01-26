9572e15501804a80d01aa51c1371623877e3a720
Matt Rice <matthew.rice.d@gmail.com>
2024-01-25 23:23:20 -0500

    Users enter themselves in database and host sees joined users

--- src/routes/HostGame.tsx
+++ src/routes/HostGame.tsx
@@ -1,12 +1,44 @@
-
+import { useState, useEffect } from "react";
 import QRCode from "../components/QRCode";
-
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+import { auth, db } from "../firebase";
 
 const HostGame = () => {
+  const params = useParams();
+  const [users, setUsers] = useState<string[]>([]);
+  useEffect(() => {
+    if (!auth.currentUser) {
+      return;
+    }
+
+    const watchUsers = () => {
+      const ref = `rooms/${params.gameId}/users`;
+      const q = query(collection(db, ref), where("status", "==", "active"));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const userSnaps: string[] = [];
+        querySnapshot.forEach((doc) => {
+          userSnaps.push(doc.data().uid);
+        });
+        setUsers(userSnaps);
+      });
+      return unsubscribe;
+    };
+
+    const unsubscribe = watchUsers();
+    return unsubscribe;
+  }, []);
   return (
     <>
       <div>HostGame</div>
-      <QRCode url={window.location.href+"/"+"play"} />
+      <p>Game ID: {params.gameId}</p>
+      <QRCode url={window.location.href + "/" + "play"} />
+      <h3>Participants</h3>
+      <ul>
+        {users.map((user) => (
+          <li>{user}</li>
+        ))}
+      </ul>
     </>
   );
 };
--- src/routes/PlayGame.tsx
+++ src/routes/PlayGame.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { setDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase";
 
 const PlayGame = () => {
-  return (
-    <div>PlayGame</div>
-  )
-}
+  const params = useParams();
+  useEffect(() => {
+    const uid = auth.currentUser?.uid;
+    if (!uid) {
+      console.log(
+        "You must be logged in to play a game or at least have a valid session."
+      );
+      return;
+    }
 
-export default PlayGame
+    const joinRoom = async () => {
+      const ref = `rooms/${params.gameId}/users/${uid}`;
+      console.log(ref);
+      await setDoc(doc(db, ref), {
+        uid: uid,
+        timeJoined: new Date().getTime(),
+        status: "active",
+      });
+    };
+
+    joinRoom();
+  }, []);
+  return <div>PlayGame {params.gameId}</div>;
+};
+
+export default PlayGame;
