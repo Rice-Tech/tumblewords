ed7fbd3077b7fc1385930feee5f94c211d3fbe1d
Matt Rice <matthew.rice.d@gmail.com>
2024-01-25 21:31:09 -0500

    Removed unused import blocking build

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,5 +1,5 @@
 import { Html } from "@react-three/drei";
-import { MeshProps, useFrame } from "@react-three/fiber";
+import { MeshProps } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { Mesh } from "three";
 
