e116fd0abe81a8ee2fde3759c185479a6f2c3e24
Josh Batugo <batugojoshua@gmail.com>
2024-01-28 19:30:16 +0000

    continue with lack of words words
    Co-authored-by: Tojo Rabemananjara <tojo-rabemananjara@users.noreply.github.com>

--- src/components/StoryTemplate.tsx
+++ src/components/StoryTemplate.tsx
@@ -15,7 +15,18 @@ const StoryTemplate = ({ template, words, revealIndex }: Props) => {
         if (!(i % 2)) {
           return <Fragment key={i}>{part}</Fragment>;
         } else {
-          if (wordIndex < words.length) {
+          if (wordIndex < words.length) {   
+            let retWord = "";         
+            const retWords = words.filter((word) => {
+              return word.index == wordIndex;
+            })
+            
+            if (!retWords[0]) {
+              retWord = "something just to fill in"
+            }  else {
+              retWord = retWords[0].word
+            }
+
             return (
               <span
                 key={`filledWord-${i}`}
@@ -26,9 +37,7 @@ const StoryTemplate = ({ template, words, revealIndex }: Props) => {
                 }
               >
                 {
-                  words.filter((word) => {
-                    return word.index == wordIndex;
-                  })[0].word
+                    retWord
                 }{" "}
                 <div className="posTooltip" hidden>
                   {part}
