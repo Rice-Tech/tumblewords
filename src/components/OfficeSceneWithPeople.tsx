16499edd2549ac965036627214910b6daca583f1
Matt Rice <matthew.rice.d@gmail.com>
2024-01-28 19:06:53 -0500

    Removed unused imports

--- src/components/OfficeSceneWithPeople.tsx
+++ src/components/OfficeSceneWithPeople.tsx
@@ -4,7 +4,6 @@ Command: npx gltfjsx@6.2.16 public/OfficeSceneWithPeople.glb -t
 */
 
 import * as THREE from 'three'
-import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
 
@@ -29,7 +28,6 @@ type GLTFResult = GLTF & {
   }
 }
 
-type ContextType = Record<string, React.ForwardRefExoticComponent<JSX.IntrinsicElements['mesh']>>
 
 export function OfficeSceneWithPeople(props: JSX.IntrinsicElements['group']) {
   const { nodes, materials } = useGLTF('/OfficeSceneWithPeople.glb') as GLTFResult
