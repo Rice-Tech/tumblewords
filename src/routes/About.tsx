5a4f86c3c3da866cfc2694e44e1471c89ffd08d9
Matt Rice <matthew.rice.d@gmail.com>
2024-02-04 14:36:08 -0500

    Initial about page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,7 +1,63 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+const teamData = [
+  {
+    name: "Matt Rice",
+    role: "Coding Team Lead",
+    contributions:
+      "Backend, Story Templating, Website, and collaborated on 3D game",
+    links: "www.ricecodes.com",
+  },
+  {
+    name: "Steven Ostuni",
+    role: "Art Team Lead",
+    contributions: "3D Scene, Character designs",
+    links: "",
+  },
+  {
+    name: "Josh Batugo",
+    role: "Coding Team",
+    contributions: "Collaborated on 3D game",
+    links: "",
+  },
+  {
+    name: "Tojo",
+    role: "Coding Team",
+    contributions: "Collaborated on 3D game",
+    links: "",
+  },
+  {
+    name: "Joel",
+    role: "Art Team",
+    contributions: "2D Art",
+    links: "",
+  },
+  {
+    name: "Matt",
+    role: "Art Team",
+    contributions: "2D Art",
+    links: "",
+  },
+];
+
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      About
+      {teamData.map((member) => (
+        <Card className=" w-2/3">
+          <CardHeader>
+            <CardTitle>{member.name}</CardTitle>
+            <CardDescription>{member.role}</CardDescription>
+            
+          </CardHeader>
+          <CardContent>
+            Contributions: {member.contributions}
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
 
-export default About
+export default About;
